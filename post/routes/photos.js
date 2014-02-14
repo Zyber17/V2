3445b6a4008ff43199d4b3dea02d8c141947b8bc
Zackary Corbett <Zyber17@gmail.com>
2014-02-14 10:01:49 -0500

    Added dev check for dev S3 bucket

--- post/routes/photos.js
+++ post/routes/photos.js
@@ -55,6 +55,11 @@
         S3_BUCKET_NAME = 'torch_photos';
         S3_ACCESS_KEY = '***REMOVED***';
         S3_SECRET_KEY = '***REMOVED***';
+        if (process.env.NODE_ENV === 'localdev') {
+          S3_BUCKET_NAME = 'V2_test';
+          S3_ACCESS_KEY = 'AKIAIX3NWOKW26ASJHVQ';
+          S3_SECRET_KEY = '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';
+        }
         name = crypto.createHmac("sha1", S3_SECRET_KEY).update("" + name + ": " + mime + " at " + (new Date().getTime()) + " random is " + (Math.floor(Math.random() * 99999999).toString())).digest("base64").replace('=', '_').replace('/', '__');
         expires = moment().add('minutes', 15).unix();
         amzHeaders = "x-amz-acl:public-read";
