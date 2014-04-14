4bd9563fc9071369f053fd1c10ed5e94961eef7f
Zackary Corbett <Zyber17@gmail.com>
2014-04-14 12:56:18 -0400

    Updated for new digital release

--- post/app.js
+++ post/app.js
@@ -74,7 +74,7 @@
     */
 
     app.get('/issues/current', function(req, res, next) {
-      return res.redirect('https://s3.amazonaws.com/torch_issues/' + (moment() < moment("2014-3-7") ? 'january_2014' : 'march_2014') + '.pdf');
+      return res.redirect('https://s3.amazonaws.com/torch_issues/' + (moment() < moment("2014-4-17") ? 'march_2014' : 'april_2014') + '.pdf');
     });
     app.get('/login', auth.login_get);
     app.post('/login', auth.login_post);
